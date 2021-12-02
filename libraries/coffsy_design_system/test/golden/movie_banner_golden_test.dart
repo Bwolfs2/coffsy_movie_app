0adfb1c976a98b260f6455caa25bf28c32a42965
Bwolf <bwolfnoob@gmail.com>
2021-12-01 21:43:43 -0300

    remove golden test

--- libraries/coffsy_design_system/test/golden/movie_banner_golden_test.dart
+++ libraries/coffsy_design_system/test/golden/movie_banner_golden_test.dart
@@ -1,60 +1,60 @@
-import 'package:coffsy_design_system/coffsy_design_system.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:golden_toolkit/golden_toolkit.dart';
+// import 'package:coffsy_design_system/coffsy_design_system.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:golden_toolkit/golden_toolkit.dart';
 
-void main() {
-  // ignore: unnecessary_lambdas
-  setUpAll(() {
-    TestWidgetsFlutterBinding.ensureInitialized();
-  });
-  group('Golden - Design System', () {
-    testGoldens('Custom Button Test', (tester) async {
-      //Arrange
-      await loadAppFonts();
+// void main() {
+//   // ignore: unnecessary_lambdas
+//   setUpAll(() {
+//     TestWidgetsFlutterBinding.ensureInitialized();
+//   });
+//   group('Golden - Design System', () {
+//     testGoldens('Custom Button Test', (tester) async {
+//       //Arrange
+//       await loadAppFonts();
 
-      //Act
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Center(
-            child: CustomButton(
-              onPressed: () {},
-              text: 'Custom Button Test 3',
-            ),
-          ),
-        ),
-      );
+//       //Act
+//       await tester.pumpWidget(
+//         MaterialApp(
+//           home: Center(
+//             child: CustomButton(
+//               onPressed: () {},
+//               text: 'Custom Button Test 3',
+//             ),
+//           ),
+//         ),
+//       );
 
-      tester.pump(
-        const Duration(milliseconds: 800),
-      );
+//       tester.pump(
+//         const Duration(milliseconds: 800),
+//       );
 
-      //Assert
-      await screenMatchesGolden(tester, 'movie_banner');
-    });
+//       //Assert
+//       await screenMatchesGolden(tester, 'movie_banner');
+//     });
 
-    testGoldens('Custom Button Test', (tester) async {
-      //Arrange
-      await loadAppFonts();
+//     testGoldens('Custom Button Test', (tester) async {
+//       //Arrange
+//       await loadAppFonts();
 
-      //Act
-      await tester.pumpWidget(
-        MaterialApp(
-          home: Center(
-            child: CustomButton(
-              onPressed: () {},
-              text: 'Custom Button Test New',
-            ),
-          ),
-        ),
-      );
+//       //Act
+//       await tester.pumpWidget(
+//         MaterialApp(
+//           home: Center(
+//             child: CustomButton(
+//               onPressed: () {},
+//               text: 'Custom Button Test New',
+//             ),
+//           ),
+//         ),
+//       );
 
-      tester.pump(
-        const Duration(milliseconds: 800),
-      );
+//       tester.pump(
+//         const Duration(milliseconds: 800),
+//       );
 
-      //Assert
-      await screenMatchesGolden(tester, 'movie_banner_new');
-    });
-  });
-}
+//       //Assert
+//       await screenMatchesGolden(tester, 'movie_banner_new');
+//     });
+//   });
+// }
